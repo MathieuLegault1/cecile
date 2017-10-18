3adb4d029fc467dc1dde197aa8001bcba36c4e63
Hugo Lefrancois <hlefrancois@mirego.com>
2017-10-18 00:42:58 -0400

    Debug

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
+require('dotenv').config()
 var express = require('express')
+var bodyParser = require('body-parser')
 var app = express()
 
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
+
 // respond with "hello world" when a GET request is made to the homepage
-app.get('/', function(req, res) {
-  res.send('hello world')
+app.post('/responses', function(request, response) {
+  console.log(request.body);
+  response.send(request.body);
 })
 
 app.listen(process.env.PORT, function() {
